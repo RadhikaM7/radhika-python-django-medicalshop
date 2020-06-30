2177d264ca877bcaf11bdcfa892da023beb2b2fb
Radhika <rmaheshwari1@hawk.iit.edu>
2020-06-30 13:11:56 -0500

    Commit 1 - Welcome and Medicine Models and Views

--- medicalshop/medicine/urls.py
+++ medicalshop/medicine/urls.py
@@ -2,7 +2,9 @@ from django.urls import path, include
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='medicine'),
+    path('<str:batchnumber>/', views.detail,name='detail')
     #url(r'^$', views.index, name='medicine')
 ]
