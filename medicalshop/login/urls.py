e52b0bf392f2d20280b2fd80bad6d339c003a803
Radhika <rmaheshwari1@hawk.iit.edu>
2020-06-30 16:29:32 -0500

    Commit 2 - Login Signup and Logout Functionality

--- medicalshop/login/urls.py
+++ medicalshop/login/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns =[
-    path('login/', views.login, name='login'),
-    path('', views.index, name='signup'),
+    path('login/', views.loginuser, name='login'),
+    path('logout/', views.logoutuser, name='logout'),
+    path('', views.signupuser, name='signup'),
+
     #url(r'^/login/$',views.login,name='login'),
     #url(r'^$', views.index, name='signup')
 ]
